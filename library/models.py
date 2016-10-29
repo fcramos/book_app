5acd9bf192f6d9c26e20e4d839c5e6ba7e9e2f7a
Felipe Corrêa Ramos <lype086@gmail.com>
2016-10-29 01:27:47 -0200

    Entidade Book

--- library/models.py
+++ library/models.py
@@ -24,3 +24,20 @@ class Category(models.Model):
 
     def __str__(self):
         return self.description
+
+
+class Book(models.Model):
+    title = models.CharField(_('título'), max_length=60)
+    author = models.ForeignKey(Author)
+    category = models.ForeignKey(Category)
+    pages = models.PositiveIntegerField(_('número de páginas'))
+    description = models.TextField(_('descrição'), blank=True, null=True)
+    published_in = models.DateField(_('publicado em'))
+
+    class Meta:
+        verbose_name = _('Livro')
+        verbose_name_plural = _('Livros')
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
--- library/tests.py
+++ library/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from library.models import Author, Category
+from datetime import datetime
+from library.models import Author, Category, Book
 
 
 class AuthorTest(TestCase):
@@ -32,3 +33,34 @@ class CategoryTest(TestCase):
     def test_str(self):
         'Category string representation may be the name.'
         self.assertEqual('Comedy', str(self.category))
+
+
+class BookTest(TestCase):
+    def setUp(self):
+        self.author = Author(
+            name='John Doe'
+        )
+        self.author.save()
+
+        self.category = Category(
+            description='Comedy',
+        )
+        self.category.save()
+
+        self.book = Book(
+            title='Any book',
+            author=self.author,
+            category=self.category,
+            pages=900,
+            description='',
+            published_in=datetime.today()
+        )
+        self.book.save()
+
+    def test_create(self):
+        'Book instance may be saved'
+        self.assertEqual(1, self.book.pk)
+
+    def test_str(self):
+        'Book string representation may be the name.'
+        self.assertEqual('Any book', str(self.book))
